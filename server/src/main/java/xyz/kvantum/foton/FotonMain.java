aacc39d929a9211e50625d3c5a526aa3ca2a8df1
Alexander Söderberg <citymonstret@hotmail.com>
2017-11-10 22:04:47 +0100

    Make project read environment variable for home path

--- server/src/main/java/xyz/kvantum/foton/FotonMain.java
+++ server/src/main/java/xyz/kvantum/foton/FotonMain.java
@@ -5,6 +5,7 @@ import com.github.intellectualsites.kvantum.api.util.RequestManager;
 import com.github.intellectualsites.kvantum.implementation.DefaultLogWrapper;
 import com.github.intellectualsites.kvantum.implementation.ServerContext;
 import com.github.intellectualsites.kvantum.implementation.error.KvantumException;
+import org.apache.commons.lang3.SystemUtils;
 
 import java.io.File;
 import java.util.Optional;
@@ -12,10 +13,27 @@ import java.util.Optional;
 public class FotonMain
 {
 
+    private static final String HOME_KEY = "FOTON_HOME";
+
     public static void main(final String[] args) throws Exception
     {
+        String homePath = null;
+
+        if ( !System.getenv().containsKey( HOME_KEY ) )
+        {
+            System.out.printf( "FATAL: Environment variable [%s] not set!\n", HOME_KEY );
+            System.exit( -1 );
+        } else
+        {
+            homePath = System.getenv( HOME_KEY );
+            if ( SystemUtils.IS_OS_UNIX && !homePath.startsWith( "/" ) )
+            {
+                homePath = "/" + homePath;
+            }
+        }
+
         final Optional<Kvantum> serverOptional = ServerContext.builder()
-                .coreFolder( new File( "./server/web/" ) )
+                .coreFolder( new File( homePath ) )
                 .logWrapper( new DefaultLogWrapper() )
                 .router( RequestManager.builder().build() )
                 .standalone( true )
@@ -28,10 +46,6 @@ public class FotonMain
         }
 
         final Kvantum server = serverOptional.get();
-
-        server.getRouter().clear();
-
-
         server.start();
     }
 
